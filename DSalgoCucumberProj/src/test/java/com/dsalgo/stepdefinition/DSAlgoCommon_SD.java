3c9da5abb9ae058525db5b5fb039caa31ae03984
unknown <auxilia06@gmail.com>
2024-10-28 13:43:33 -0700

    Added login files pom,SD and feature

--- DSalgoCucumberProj/src/test/java/com/dsalgo/stepdefinition/DSAlgoCommon_SD.java
+++ DSalgoCucumberProj/src/test/java/com/dsalgo/stepdefinition/DSAlgoCommon_SD.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 import com.dsalgo.pom.DataStructure_IntroductionPom;
 import com.dsalgo.pom.DSAlgoHomePom;
+import com.dsalgo.pom.DSAlgoLoginPom;
 import com.dsalgo.pom.DSAlgoGetStartedPom;
 import com.dsalgo.pom.DSAlgoSignInPom;
 import com.dsalgo.pom.DSAlgoStackPom;
@@ -13,6 +14,7 @@ public class DSAlgoCommon_SD {
 	public WebDriver driver;
 	public DSAlgoGetStartedPom getstartedpage_obj;
 	public DSAlgoSignInPom signinpage_obj;
+	public DSAlgoLoginPom loginpage_obj;
 	public DSAlgoHomePom homepage_obj;
 	public DSAlgoStackPom stakpage_obj;
 	public DataStructure_IntroductionPom datastructurepage_obj;
