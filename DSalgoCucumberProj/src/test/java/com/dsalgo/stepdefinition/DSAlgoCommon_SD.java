7dae46c4fd5a463f0437f56328de3b9455093ab7
Pavithra-Subburaj <pavithrasubburaj@gmail.com>
2024-11-01 17:12:29 -0500

    hooks and updates

--- DSalgoCucumberProj/src/test/java/com/dsalgo/stepdefinition/DSAlgoCommon_SD.java
+++ DSalgoCucumberProj/src/test/java/com/dsalgo/stepdefinition/DSAlgoCommon_SD.java
@@ -2,11 +2,11 @@ package com.dsalgo.stepdefinition;
 
 import org.openqa.selenium.WebDriver;
 
-import com.dsalgo.pom.DataStructure_IntroductionPom;
-import com.dsalgo.pom.DSAlgoHomePom;
 import com.dsalgo.pom.DSAlgoGetStartedPom;
+import com.dsalgo.pom.DSAlgoHomePom;
 import com.dsalgo.pom.DSAlgoSignInPom;
 import com.dsalgo.pom.DSAlgoStackPom;
+import com.dsalgo.pom.DataStructure_IntroductionPom;
 
 public class DSAlgoCommon_SD {
 
@@ -16,5 +16,6 @@ public class DSAlgoCommon_SD {
 	public DSAlgoHomePom homepage_obj;
 	public DSAlgoStackPom stakpage_obj;
 	public DataStructure_IntroductionPom datastructurepage_obj;
+	
 	
 }
